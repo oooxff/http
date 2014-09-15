5c28d0cb3ed8836e1eefcfa11069a28f9c15242d
oooxff <oooxff@126.com>
2014-09-15 15:44:20 +0800

    Add test sample code for get host by name.
    Signed-off-by: oooxff <oooxff@126.com>

--- test/get_host_by_name.c
+++ test/get_host_by_name.c
@@ -7,34 +7,21 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 
 #include <netdb.h>
 
 int main(int argc, char *argv[])
 {
-    int i;
     struct hostent *host;
+    struct in_addr ip;
 
     host = gethostbyname("www.baidu.com");
     assert(host);
     
     printf("h_name: %s\n", host->h_name);
-
-    for (i = 0; host->h_aliases[i]; i ++) {
-        printf("h_aliases[%d] = %s\n", i, host->h_aliases[i]);
-    }
-
-    printf("h_length = %d\n", host->h_length);
-
-    switch(host->h_addrtype) {
-        case AF_INET:
-            printf("h_addrtype = AF_INET\n");
-
-            for (i = 0; host->h_addr_list[i]; i ++) {
-                printf("h_addr_list[%d] = %s\n", i, host->h_addr_list[i]);
-            }
-            break;
-    }
+    bcopy(host->h_addr, &(ip.s_addr), host->h_length);
+    printf("IP: %s\n", inet_ntoa(ip));
     return 0;
 }
 
